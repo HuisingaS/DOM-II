8dc88b542d910b5c29be056ee97a356f48fba4a4
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-08-05 14:41:02 -0500

    navBar listener added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+const navBar = document.querySelector('.nav')
+console.log(navBar)
+
+navBar.addEventListener('mouseover', (navMouseClick) =>{
+    navMouseClick.target.style.color = 'purple'
+    navMouseClick.target.style.border = '5px solid gold'
+})
+
+navBar.addEventListener('mouseleave', (navMouseOut) =>{
+    navMouseOut.target.textContent = "*poof* I've got you now!";
+    navMouseOut.target.style.width = '70%';
+    navMouseOut.target.style.fontSize = '5rem';
+    navMouseOut.target.style.textAlign = 'center';
+    navMouseOut.target.style.color = 'purple'
+    navMouseOut.target.style.border = '5px solid gold'   
+})
+
