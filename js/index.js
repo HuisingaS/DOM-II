615885036e8ab3a154fa242dd1b8fe39ed2dbd80
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-08-05 17:46:27 -0500

    finished mvp!

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
 // Your code goes here
 
-const navBar = document.querySelector('.nav')
-console.log(navBar)
-
-navBar.addEventListener('mouseover', (event) =>{
-    event.target.style.color = 'purple'
-    event.target.style.border = '5px solid gold'
-})
-
-navBar.addEventListener('mouseleave', (event) =>{
-    event.target.textContent = "*poof* I've got you now!";
-    event.target.style.width = '70%';
-    event.target.style.fontSize = '4rem';
-    event.target.style.textAlign = 'center';
-    event.target.style.color = 'purple'
-    event.target.style.border = '5px solid gold'
-})
-
-const heading1 = document.querySelector('.logo-heading')
-
-document.addEventListener('keydown', (item) =>{
-    if(item.key === 'Escape'){
+const heading1 = document.querySelector('.logo-heading');
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'Escape'){
         heading1.textContent = "Welcome - Please don't go!"
         heading1.style.marginTop = "1.5%"
         heading1.style.fontSize = '4rem'
@@ -28,23 +10,62 @@ document.addEventListener('keydown', (item) =>{
     }
 })
 
+document.addEventListener('keyup', function(event) {
+    if(event.key === 'Escape'){
+        heading1.textContent = "I'm begging you :("
+        heading1.style.marginTop = "1.5%"
+        heading1.style.fontSize = '4rem'
+        heading1.style.color = 'orange'
+    }
+})
 
 let img1 = document.querySelector('img')
-img1.addEventListener('mouseenter', event => {
+img1.addEventListener('mouseenter', function() {
     img1.src = "https://di-uploads-pod13.dealerinspire.com/stohlmanvolkswagen/uploads/2019/07/VWbus.jpg"
 })
 
-img1.addEventListener('mouseleave', event => {
+img1.addEventListener('mouseleave', function() {
     img1.src = "https://kleinmanenergy.upenn.edu/sites/default/files/styles/new_hero/public/policythumbs/747takeoff.png?itok=ntyL_dnV"
 })
 
+img1.addEventListener('drag', function() {
+    img1.src = "https://cdn.cnn.com/cnnnext/dam/assets/130707184658-52-san-francisco-plane-crash-horizontal-large-gallery.jpg"
+})
 
-let img2 = document.querySelector('.img-content')
-img2.addEventListener('mouseenter', event => {
-    img2.style.transform = "scale(2)"
-    img2.style.transition = "all 1s"
-  })
+const navBar = document.querySelector('.nav')
+navBar.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'purple'
+    event.target.style.border = '5px solid gold'
+})
 
-  img2.addEventListener('mouseleave', event => {
-    img2.style.transform = "scale(1.0)"
-  })
+document.addEventListener('contextmenu', (function(event) {
+    event.target.style.background = 'black'
+}))
+
+const h4MouseMove = document.querySelector('h2')
+h4MouseMove.addEventListener('mousemove', function() {
+    h4MouseMove.style.fontSize = '7rem'
+})
+
+const button = document.querySelector('.btn')
+button.addEventListener('click', function(event) {
+    event.target.style.color = 'grey'
+    event.target.style.background = "gold"
+})
+
+const footer = document.querySelector('footer')
+footer.addEventListener('mousedown', function(event) {
+    event.target.style.background = "gold"
+    event.target.style.color = "purple"
+    event.target.style.border = "5px solid black"
+})
+
+const propogation = document.querySelectorAll('img')
+propogation[0].addEventListener('mousedown', function(event) {
+    propogation[0].style.border = '10px solid black'
+    event.stopPropagation();
+})
+
+$('nav a').click(function (event) {
+    event.preventDefault();
+});
